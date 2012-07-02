a344d0036580d712e28098ea93390c65657227cc
lulalala <mark@goodlife.tw>
2012-07-02 11:56:07 +0800

    initiali commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoMigration::Application.config.secret_token = '72198391c7663f7765660ce5d9a7653ebc1f0757d91bfc00d402a85e02234b8b71f00fe863465d4dfeb1aa665f1b95105b09e521d0872054a7723a4c5a38ed09'
